e6c1606b2f214978b0d21bbdb376ff17f4d3a55b
Emanuel Krivoy <krivoy@google.com>
2021-08-20 07:03:39 -0700

    Add options parameter to createSyncAccessHandle()
    For now the only accepted (and required) option is "mode: 'in-place'".
    Having this parameter allows us to later define a more appropriate
    default behavior for access handles than the one we currently implement.
    This is specially useful in the context of expanding access handle usage
    outside of OPFS.
    
    For more details check:
    https://github.com/WICG/file-system-access/blob/main/AccessHandle.md#exposing-accesshandles-on-all-filesystems
    
    Bug: 1218431
    Change-Id: I50d0a4acfd81874e17b6b28d1c2bf446b0397a4d

--- file-system-access/sandboxed_FileSystemFileHandle-sync-access-handle-lock.https.tentative.worker.js
+++ file-system-access/sandboxed_FileSystemFileHandle-sync-access-handle-lock.https.tentative.worker.js
@@ -6,12 +6,15 @@ promise_test(async t => {
   const dir = await navigator.storage.getDirectory();
   const fileHandle = await dir.getFileHandle('OPFS.test', {create: true});
 
-  const syncHandle1 = await fileHandle.createSyncAccessHandle();
+  const syncHandle1 = await fileHandle
+                        .createSyncAccessHandle({mode: "in-place"});
   await promise_rejects_dom(
-      t, 'InvalidStateError', fileHandle.createSyncAccessHandle());
+      t, 'InvalidStateError',
+      fileHandle.createSyncAccessHandle({mode: "in-place"}));
 
   await syncHandle1.close();
-  const syncHandle2 = await fileHandle.createSyncAccessHandle();
+  const syncHandle2 = await fileHandle
+                        .createSyncAccessHandle({mode: "in-place"});
   await syncHandle2.close();
 }, 'There can only be one open access handle at any given time');
 
@@ -20,15 +23,19 @@ promise_test(async t => {
   const fooFileHandle = await dir.getFileHandle('foo.test', {create: true});
   const barFileHandle = await dir.getFileHandle('bar.test', {create: true});
 
-  const fooSyncHandle = await fooFileHandle.createSyncAccessHandle();
+  const fooSyncHandle = await fooFileHandle
+                          .createSyncAccessHandle({mode: "in-place"});
   t.add_cleanup(() => fooSyncHandle.close());
 
-  const barSyncHandle1 = await barFileHandle.createSyncAccessHandle();
+  const barSyncHandle1 = await barFileHandle
+                           .createSyncAccessHandle({mode: "in-place"});
   await promise_rejects_dom(
-      t, 'InvalidStateError', barFileHandle.createSyncAccessHandle());
+      t, 'InvalidStateError',
+      barFileHandle.createSyncAccessHandle({mode: "in-place"}));
 
   await barSyncHandle1.close();
-  const barSyncHandle2 = await barFileHandle.createSyncAccessHandle();
+  const barSyncHandle2 = await barFileHandle
+                           .createSyncAccessHandle({mode: "in-place"});
   await barSyncHandle2.close();
 }, 'An access handle from one file does not interfere with the creation of an' +
      ' access handle on another file');
