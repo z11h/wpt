f31ae1bfba7e0f78b9341b5aa5e7af589cc0ab50
Mustaq Ahmed <mustaq@google.com>
2021-09-28 12:15:08 -0700

    Added WPTs for user-activation from keypress and keyup events.
    These tests are for the following HTML PR:
    https://github.com/whatwg/html/pull/6818
    
    Also renamed the existing click event test for consistency.
    
    Change-Id: If77750f4159828b68bd91a4b48f46606421b7df6

--- html/user-activation/resources/utils.js
+++ html/user-activation/resources/utils.js
@@ -7,3 +7,26 @@ function delayByFrames(f, num_frames) {
   }
   recurse(num_frames);
 }
+
+// Returns a Promise which is resolved with the event object when the event is
+// fired.
+function waitForEvent(eventType) {
+  return new Promise(resolve => {
+    document.body.addEventListener(eventType, e => resolve(e), {once: true});
+  });
+}
+
+
+// Returns a Promise which is resolved with a "true" iff transient activation
+// was available and successfully consumed.
+//
+// This function relies on Fullscreen API to check/consume user activation
+// state.
+async function consumeTransientActivation() {
+  return new Promise(resolve => {
+    document.body.requestFullscreen()
+        .then(document.exitFullscreen())
+        .then(() => resolve(true))
+        .catch(() => resolve(false));
+  });
+}
